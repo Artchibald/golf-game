d91cbd26c415bc09e3f7f9a041dbfb8e14690171
Artchibald <archie@archibaldbutler.com>
2022-03-31 16:45:56 +0100

    initial typescript working

---
+++ golf-game/src/components/something.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Something() {
+  return (
+    <div className="something">
+      SOMETHING
+      <br />
+      SOEMTHING ELSE
+    </div>
+  );
+}
+
+export default Something;
